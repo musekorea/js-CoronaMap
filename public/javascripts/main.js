3b8d91d2ea6cc47bf1588f3d7cacf6a51038e4e2
m3use <77679025+musekorea@users.noreply.github.com>
2021-06-30 23:06:59 +0800

    views/public setup

---
+++ public/javascripts/main.js
@@ -0,0 +1,5 @@
+var mapOptions = {
+  center: new naver.maps.LatLng(37, 127),
+  zoom: 10,
+};
+var map = new naver.maps.Map('map', mapOptions);
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
 app.listen(8080, () => {
-  console.log(`Server is listening on Port 8080`);
+  console.log(`💚 Server is listening on Port 8080 `);
 });
