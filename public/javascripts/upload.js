f278249dcf0fb022c0f9afeffe5613b9fa02161b
m3use <77679025+musekorea@users.noreply.github.com>
2021-07-06 01:33:23 +0800

    kakao api

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,5 +1,6 @@
+var mapContainer = document.getElementById('map');
 var mapOptions = {
-  center: new naver.maps.LatLng(37, 127),
-  zoom: 10,
+  center: new kakao.maps.LatLng(37.57422945570032, 126.97676426853468),
+  level: 3,
 };
-var map = new naver.maps.Map('map', mapOptions);
+var map = new kakao.maps.Map(mapContainer, mapOptions);
