bdddf91f906b6182514380d8799a1425a5523682
m3use <77679025+musekorea@users.noreply.github.com>
2021-07-08 00:56:06 +0800

    search

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,6 +1,32 @@
-var mapContainer = document.getElementById('map');
-var mapOptions = {
+const mapContainer = document.getElementById('map');
+const mapOptions = {
   center: new kakao.maps.LatLng(37.57422945570032, 126.97676426853468),
   level: 3,
 };
-var map = new kakao.maps.Map(mapContainer, mapOptions);
+let map = new kakao.maps.Map(mapContainer, mapOptions);
+
+let infoWindow = new kakao.maps.InfoWindow({
+  zIndex: 1,
+});
+
+let markerList = [];
+
+let places = new kakao.maps.services.Places();
+
+function palcesSearchCallback(result, status) {
+  if (status === kakao.maps.services.Status.OK) {
+    console.log(result);
+  } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+    alert(`검색 결과가 존재하지 않습니다`);
+    return;
+  } else if (status === kakao.maps.services.Status.Error) {
+    alert(`검색중 오류가 발생했습니다`);
+    return;
+  }
+}
+const searchForm = document.querySelector('#searchForm');
+let keyword = document.querySelector('#keyword');
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  places.keywordSearch(keyword.value, palcesSearchCallback);
+});
