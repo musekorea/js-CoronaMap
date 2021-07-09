c328106ff5220f87968386e05f4e84cb3bdf85cf
m3use <77679025+musekorea@users.noreply.github.com>
2021-07-09 18:50:55 +0800

    initalize

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -4,15 +4,42 @@ const mapOptions = {
   level: 3,
 };
 let map = new kakao.maps.Map(mapContainer, mapOptions);
-let infoWindow = new kakao.maps.InfoWindow({
-  zIndex: 1,
-});
 let places = new kakao.maps.services.Places();
 let markerList = [];
+let infoWindow = new kakao.maps.InfoWindow();
+const placesList = document.querySelector('#placesList');
 
+//=================SEARCH=======================================
+function placesSearchCallback(result, status) {
+  if (status === kakao.maps.services.Status.OK) {
+    removeList();
+    removeMarker();
+    makeList(result);
+  } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+    alert(`검색 결과가 존재하지 않습니다`);
+    return;
+  } else if (status === kakao.maps.services.Status.Error) {
+    alert(`검색중 오류가 발생했습니다`);
+    return;
+  }
+}
+//==================INITALIZE======================================
+function removeList() {
+  while (placesList.hasChildNodes()) {
+    placesList.removeChild(placesList.firstChild);
+  }
+}
+
+function removeMarker() {
+  for (let i = 0; i < markerList.length; i++) {
+    markerList[i].setMap(null);
+  }
+}
+
+//==================LIST============================================
 function makeList(result) {
-  const placesList = document.querySelector('#placesList');
   //let bounds = new kakao.maps.LatLngBounds();
+
   for (let i = 1; i < result.length; i++) {
     let lat = result[i].y;
     let lng = result[i].x;
@@ -26,38 +53,51 @@ function makeList(result) {
     });
     marker.setMap(map);
     markerList.push(marker);
-    console.log(markerList);
 
     const listDiv = document.createElement('div');
     listDiv.className = 'item';
     listDiv.innerHTML = `
-      <div class="info">
-        <div class="infoTitle">
+      <div class="listContainer">
+        <div class="listTitle">
           ${placeName}
         </div>
         <span>${address}</span>
       </div>
       <hr>
       `;
-
     placesList.appendChild(listDiv);
+
+    kakao.maps.event.addListener(marker, 'click', () => {
+      infoWindow.close();
+      paintInfoWindow(marker, placeName, address, lat, lng);
+    });
+
+    kakao.maps.event.addListener(map, 'click', () => {
+      infoWindow.close();
+    });
   }
+}
+//===========INFO WINDOW=========================================
+
+function paintInfoWindow(marker, placeName, address, lat, lng) {
+  infoWindow = new kakao.maps.InfoWindow({
+    position: new kakao.maps.LatLng(lat, lng),
+    content: `
+      <div class="infoWindow">
+        ${placeName}<br>
+        ${address}<br>
+        <button>등록</button>
+      </div>`,
+    zIndex: 1,
+  });
+
+  infoWindow.open(map, marker);
 }
 
-function palcesSearchCallback(result, status) {
-  if (status === kakao.maps.services.Status.OK) {
-    makeList(result);
-  } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-    alert(`검색 결과가 존재하지 않습니다`);
-    return;
-  } else if (status === kakao.maps.services.Status.Error) {
-    alert(`검색중 오류가 발생했습니다`);
-    return;
-  }
-}
+//==========================BUTTON=================================
 const searchForm = document.querySelector('#searchForm');
 let keyword = document.querySelector('#keyword');
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  places.keywordSearch(keyword.value, palcesSearchCallback);
+  places.keywordSearch(keyword.value, placesSearchCallback);
 });
